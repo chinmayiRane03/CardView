27a8d681f921b1f05ad164918a1044667988828b
chinmayiRane <chinmayi.rane@spit.ac.in>
2020-03-11 22:33:20 +0530

    //InitialCommit

---
+++ app/src/main/java/com/example/view/CustomeAdapter.java
@@ -0,0 +1,66 @@
+package com.example.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+
+
+import java.util.ArrayList;
+
+public class CustomeAdapter extends RecyclerView.Adapter<CustomeAdapter.MyViewHolder> {
+
+    private ArrayList<DataModel> dataSet;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewName;
+        TextView textViewVersion;
+        ImageView imageViewIcon;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            this.textViewName = (TextView) itemView.findViewById(R.id.address);
+            this.textViewVersion = (TextView) itemView.findViewById(R.id.days);
+            this.imageViewIcon = (ImageView) itemView.findViewById(R.id.main);
+        }
+    }
+
+    public CustomeAdapter(ArrayList<DataModel> data) {
+        this.dataSet = data;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                           int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cards_layout, parent, false);
+
+        view.setOnClickListener(MainActivity.myOnClickListener);
+
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int listPosition) {
+
+        TextView textViewName = holder.textViewName;
+        TextView textViewVersion = holder.textViewVersion;
+        ImageView imageView = holder.imageViewIcon;
+
+        textViewName.setText(dataSet.get(listPosition).getName());
+        textViewVersion.setText(dataSet.get(listPosition).getVersion());
+        imageView.setImageResource(dataSet.get(listPosition).getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+}
---
+++ app/src/main/java/com/example/view/MyData.java
@@ -0,0 +1,12 @@
+package com.example.view;
+
+public class MyData {
+
+    static String[] nameArray = {"Andheri", "Kandivali", "Malad", "Juhu", "Goregaon", "Dadar", "Matunga","CST", "Andheri", "Bhavan", "Seawoods"};
+    static String[] versionArray = {"3 days", "6 days", "9 days", "10 days", "5 days", "3 days", "3 days", "7 days", "5 days", "2 days","8 days"};
+
+    static Integer[] drawableArray = {R.drawable.bmc, R.drawable.bmc, R.drawable.bmc,
+            R.drawable.bmc,R.drawable.bmc,R.drawable.bmc,R.drawable.bmc,R.drawable.bmc,R.drawable.bmc,R.drawable.bmc,R.drawable.bmc};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}
